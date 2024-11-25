5b879660dfdf6e50e79f0590cf60797d1b9169f6
= <=>
2024-11-25 19:40:40 +0100

    cleanup

--- processDataset.py
+++ processDataset.py
@@ -2,20 +2,19 @@ import pandas as pd
 
 data = pd.read_csv("tiny-dataset.csv")
 data.drop(["metadata", "prompt"], inplace=True, axis=1)
-# data = data.iloc[:100]
-
-# print(data.head())
 
 # Now, let's embed the text using the model
 from sentence_transformers import SentenceTransformer
 
 model = SentenceTransformer("Snowflake/snowflake-arctic-embed-s")
 
-batch_size = 128  # Adjust batch size based on your GPU/CPU memory
+batch_size = 32  # Adjust batch size based on your GPU/CPU memory
 texts = data["text"].tolist()
 embeddings = model.encode(texts, batch_size=batch_size, show_progress_bar=True)
 
 data["embedding"] = embeddings.tolist()
+tmp_text = data["text"].tolist()[:10]
+tmp_emb = data["embedding"].tolist()[:10]
 data.drop("text", inplace=True, axis=1)
 
 print(data.head())
