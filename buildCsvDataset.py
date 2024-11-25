1bba51b08e8c2a7035777bd8c598c1d641ce3a32
= <=>
2024-11-25 21:01:14 +0100

    Squash merge one-hot-encode-labels into main

--- buildCsvDataset.py
+++ buildCsvDataset.py
@@ -8,8 +8,8 @@ fine_web_ds = load_dataset(
     "HuggingFaceFW/fineweb-edu-llama3-annotations", split="train", streaming=True
 )
 
-# Take only the first 1000 samples
-num_samples = 1000
+# Take only the first 4000 samples
+num_samples = 4000
 sampled_data = list(islice(fine_web_ds, num_samples))
 
 df = pd.DataFrame(sampled_data)
