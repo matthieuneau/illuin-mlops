84248296432eca5cac3b258a72dde11e25779f8e
= <=>
2025-01-17 12:19:40 +0100

    update local config. almost functional. need to fix bashrc issue

--- rayServe.py
+++ rayServe.py
@@ -3,21 +3,22 @@ from fastapi import FastAPI
 from ray import serve
 import torch
 
-ray.init(
-    address="ray://34.145.33.35:10001"
-)  # todo: check it is the correct head node ip
+# TODO: Remove runtime_env when deploying to cloud
+ray.init(address="auto")
+
+# TODO: check if needed to specify address
 serve.start(http_options={"host": "0.0.0.0", "port": 8000})
 
 app = FastAPI()
-
-# Load the model once during app startup
-model = torch.jit.load("models/classifier.pt")
-model.eval()  # Set the model to evaluation mode
 
 
 @serve.deployment
 @serve.ingress(app)
 class MyFastAPIDeployment:
+    def __init__(self) -> None:
+        self.model = torch.jit.load("~/models/classifier.pt")
+        self.model.eval()  # Set the model to evaluation mode
+
     @app.get("/")
     def root(self):
         return "Hello, world!"
@@ -26,7 +27,7 @@ class MyFastAPIDeployment:
     async def predict(self, input_data: list[float]):
         input = torch.tensor(input_data, dtype=torch.float32)
         with torch.no_grad():
-            output = model(input)
+            output = self.model(input)
         return {"output": output.tolist()}
 
 
